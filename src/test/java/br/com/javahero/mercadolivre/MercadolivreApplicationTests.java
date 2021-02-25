af79e3a0f28fc2588bd0c8102e970e1677cfd8f2
Cassio Murilo <cmurilo.san@gmail.com>
2021-02-25 09:29:52 -0300

    Initial commit

---
+++ src/test/java/br/com/javahero/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javahero.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
