8f8cc25da5df13219b9073f70751db473cf150ab
Cassio Murilo <cmurilo.san@gmail.com>
2021-03-02 09:15:09 -0300

    Add dependencies

--- src/main/java/br/com/javahero/mercadolivre/model/User.java
+++ src/main/java/br/com/javahero/mercadolivre/model/User.java
@@ -1,9 +1,17 @@
 package br.com.javahero.mercadolivre.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 // Model
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private LocalDateTime createdAt;
     private String email;
     private String password;
