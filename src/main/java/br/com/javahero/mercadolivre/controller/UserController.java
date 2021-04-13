578e7908bb4d9eb7a43288b61cb652ac29f1b2db
Cassio Murilo <cmurilo.san@gmail.com>
2021-04-13 09:44:08 -0300

    Não podemos ter dois usuários com o mesmo email.

--- src/main/java/br/com/javahero/mercadolivre/controller/UserController.java
+++ src/main/java/br/com/javahero/mercadolivre/controller/UserController.java
@@ -4,6 +4,7 @@ import br.com.javahero.mercadolivre.dto.UserForm;
 import br.com.javahero.mercadolivre.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,6 +25,10 @@ public class UserController {
 
     @PostMapping("/user")
     public String newUser(@Valid UserForm userForm, BindingResult bindingResult) {
+        if(userRepository.existsByEmail(userForm.getLogin())) {
+            bindingResult.addError(new FieldError("userForm", "login",
+                    "Email duplicado"));
+        }
         if(bindingResult.hasErrors()) {
             System.out.println(bindingResult.getAllErrors());
             return "user/new";
