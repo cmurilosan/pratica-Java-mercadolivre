6611cea5c4ee017bcb3ed3c8655cd0abc4601761
Wanderson M <wandersonmaceds@gmail.com>
2021-04-13 10:24:33 -0300

    adicionando regra de unicidade do usuário e corrigindo validações de senha

--- src/main/java/br/com/javahero/mercadolivre/controller/UserController.java
+++ src/main/java/br/com/javahero/mercadolivre/controller/UserController.java
@@ -2,11 +2,13 @@ package br.com.javahero.mercadolivre.controller;
 
 import br.com.javahero.mercadolivre.dto.UserForm;
 import br.com.javahero.mercadolivre.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -24,17 +26,18 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    public String newUser(@Valid UserForm userForm, BindingResult bindingResult) {
+    public ModelAndView newUser(@Valid UserForm userForm, BindingResult bindingResult) {
+
         if(userRepository.existsByEmail(userForm.getLogin())) {
             bindingResult.addError(new FieldError("userForm", "login",
                     "Email duplicado"));
         }
         if(bindingResult.hasErrors()) {
-            System.out.println(bindingResult.getAllErrors());
-            return "user/new";
+            return new ModelAndView("user/new", HttpStatus.BAD_REQUEST);
         }
 
         this.userRepository.save(userForm.toEntity());
-        return "user/created";
+        return new ModelAndView("user/created");
+
     }
 }
--- src/main/java/br/com/javahero/mercadolivre/dto/UserForm.java
+++ src/main/java/br/com/javahero/mercadolivre/dto/UserForm.java
@@ -3,7 +3,9 @@ package br.com.javahero.mercadolivre.dto;
 import br.com.javahero.mercadolivre.model.User;
 import br.com.javahero.mercadolivre.security.PasswordEncoder;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class UserForm {
 
@@ -16,22 +18,19 @@ public class UserForm {
     private String password;
 
     public User toEntity() {
-        return new User(login, password);
+        return new User(login, PasswordEncoder.encode(password));
     }
 
     public String getLogin() {
         return login;
     }
+    public String getPassword() { return password; }
 
     public void setLogin(String login) {
         this.login = login;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
-        this.password = PasswordEncoder.encode(password);
+        this.password = password;
     }
 }
